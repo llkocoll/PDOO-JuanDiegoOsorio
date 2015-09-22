df46abf611881947b0a77784b2b0ef19d3ad5b1e
Juan Diego Osorio Restrepo <honey.nutty@hotmail.com>
2015-09-22 18:25:58 -0500

    Subida Reto Semana 11

---
+++ Semana 11/Threads/Threads/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Thread thread1 = new Thread (CountTo100);
+			thread1.Start ("Thread 1");
+
+			Thread thread2 = new Thread (CountTo100);
+			thread2.Start ("Thread 2");
+
+			thread1.Join ();
+			thread2.Join ();
+
+			Console.ReadKey ();
+		}
+
+		public static void CountTo100(object data)
+		{
+			string threadName = (string)data;
+			for (int index = 0; index < 100; index++)
+				Console.WriteLine (threadName + ": "+ (index + 1));
+			
+		}
+	}
+}
